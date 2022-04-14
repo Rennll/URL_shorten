140bfad2b316a3ecbe695e550cc90932176cac34
Rennll <dt616717@gmail.com>
2022-04-15 00:58:19 +0800

    feat: setting database by mongoose

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 3000
+
+// setting database by mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', err => console.error(err))
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
 
 // setting routes
 app.get('/', (req, res) => {
