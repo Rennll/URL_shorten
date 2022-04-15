8163183c0574d66819be5e14e20387db75ca25a4
Rennll <dt616717@gmail.com>
2022-04-15 21:35:33 +0800

    feat: add short url copy btn by clipboard.js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,5 +1,11 @@
 const form = document.querySelector('.needs-validation')
 const submitButton = document.querySelector('.needs-validation button')
+const clipboard = new ClipboardJS('.btn-copy', {
+  target (trigger) {
+    // trigger 參數為觸發元素 Node
+    return document.querySelector('#short-url')
+  }
+})
 
 form.addEventListener('submit', function onFormSubmitted (event) {
   if (!form.checkValidity()) {
