e8b75fc4842aadfb4321035fed59a611758b2f8a
Rennll <dt616717@gmail.com>
2022-04-15 21:12:19 +0800

    refactor: modify express routes & fix: add do while in routes/modules/home to prevent repeat short url

--- app.js
+++ app.js
@@ -1,66 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
-const Record = require('./models/record')
-const randomChars = require('./controllers/randomChars')
+const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
 const PORT = 3000
-const SHORTEN_CHARS_LEN = 5
-const BASE_URL = 'http://localhost:3000/'
 
 // setting template and view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// using express-bodyParser, static file, routes
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
-// setting routes
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  const url = req.body.url.trim()
-
-  Record.find({ url })
-    .lean()
-    .then(record => {
-      if (record.length) {
-        const shortChars = record[0].shortChars
-        res.render('shorten', { short: BASE_URL + shortChars })
-      } else {
-        const shortChars = randomChars(SHORTEN_CHARS_LEN)
-
-        Record.find({ shortChars })
-          .then(record => {
-            if (!record.length) {
-              Record.create({ url, shortChars })
-                .then(() => {
-                  res.render('shorten', { short: BASE_URL + shortChars })
-                })
-                .catch(err => console.error(err))
-            }
-          })
-      }
-    })
-    .catch(err => console.error(err))
-})
-
-app.get('/:shortChars', (req, res) => {
-  const shortChars = req.params.shortChars
-
-  Record.find({ shortChars })
-    .then(record => {
-      if (record.length) {
-        res.redirect(record[0].url)
-      } else {
-        res.redirect('/')
-      }
-    })
-})
+app.use(routes)
 
 // start and listen
 app.listen(PORT, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+
+const Record = require('../../models/record')
+const randomChars = require('../../controllers/randomChars')
+
+const SHORTEN_CHARS_LEN = 5
+const BASE_URL = 'http://localhost:3000/'
+
+// setting index
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+// setting route for shorten url
+router.post('/', (req, res) => {
+  const url = req.body.url.trim()
+  // 首先查看是否已經轉換過
+  // 有 -> 把搜尋到的短網址渲染到結果頁
+  // 沒有 -> 進入 do ... while
+  //        先產生新的短網址
+  //        如果新的網址不存在
+  //        -> 則創造一個新的 record
+  //          -> 儲存在之後再執行渲染，並脫離迴圈
+  Record.find({ url })
+    .lean()
+    .then(record => {
+      if (record.length) {
+        const shortChars = record[0].shortChars
+        res.render('shorten', { short: BASE_URL + shortChars })
+      } else {
+        let isUnique = false
+        do {
+          const shortChars = randomChars(SHORTEN_CHARS_LEN)
+          Record.find({ shortChars })
+            .then(record => {
+              if (!record.length) {
+                Record.create({ url, shortChars })
+                  .then(() => {
+                    isUnique = true
+                    res.render('shorten', { short: BASE_URL + shortChars })
+                  })
+                  .catch(err => console.error(err))
+              }
+            })
+        } while (isUnique === true)
+      }
+    })
+    .catch(err => console.error(err))
+})
+
+// setting shorten url as params
+router.get('/:shortChars', (req, res) => {
+  const shortChars = req.params.shortChars
+
+  // 尋找使用者提供的短網址
+  // 若有則渲染，沒有就重新回到根目錄
+  Record.find({ shortChars })
+    .then(record => {
+      if (record.length) {
+        res.redirect(record[0].url)
+      } else {
+        res.redirect('/')
+      }
+    })
+})
+
+module.exports = router
